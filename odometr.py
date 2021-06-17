c531ca49e42fce4609714102da0b9ced19f03e49
MaximLKorenev <73540257+MaximLKorenev@users.noreply.github.com>
2021-06-17 21:06:51 +0300

    Update squirrel.py

---
+++ odometr.py
@@ -0,0 +1,5 @@
+def odometr(oksana):
+    distance = oksana[0] * oksana[1]
+    for i in range(2, len(oksana), 2):
+        distance += oksana[i] * (oksana[i + 1] - oksana[i - 1])
+    return distance
