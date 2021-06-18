22e4e6e829f35cc8083dc4e904026c5a5618122c
MaximLKorenev <73540257+MaximLKorenev@users.noreply.github.com>
2021-06-18 18:42:04 +0300

    Update odometr.py

--- odometr.py
+++ odometr.py
@@ -1,5 +1,4 @@
-def odometr(oksana):
-    oksana = list(oksana)
+def odometer(oksana):
     distance = oksana[0] * oksana[1]
     for i in range(2, len(oksana), 2):
         distance += oksana[i] * (oksana[i + 1] - oksana[i - 1])
