750b2af5cae91e8361ecc9da133418e88a43ae12
MaximLKorenev <mlkorenev@yandex.ru>
2021-06-20 17:50:31 +0300

    Update squirrel.py

--- PatternUnlock.py
+++ PatternUnlock.py
@@ -4,13 +4,13 @@ def PatternUnlock(N, hits):
         a = hits[i]
         b = hits[i + 1]
         if (a == 1 or a == 3) and (b == 5 or b == 8):
-            ln += 1.41
+            ln += 2 ** 0.5
         elif a == 2 and (b == 6 or b == 9 or b == 7 or b == 4):
-            ln += 1.41
+            ln += 2 ** 0.5
         elif (a == 4 or a == 7 or a == 6 or a == 9) and b == 2:
-            ln += 1.41
+            ln += 2 ** 0.5
         elif (a == 5 or a == 8) and (b == 1 or b == 3):
-            ln += 1.41
+            ln += 2 ** 0.5
         else:
             ln += 1
     ln_str_with_0 = str(int(round(ln, 5) * 100000))
