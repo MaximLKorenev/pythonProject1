2485dd1d45125e42fa0e1d801e9206b8e5d142a0
MaximLKorenev <73540257+MaximLKorenev@users.noreply.github.com>
2021-08-07 14:27:03 +0300

    Update white_walkers.py

--- white_walkers.py
+++ white_walkers.py
@@ -1,18 +1,23 @@
 def white_walkers(village):
     a = 0
     b = 0
-    for i in range(len(village) - 1):
+    i = 0
+    while i < len(village) - 2:
         if village[i].isdigit():
             m = i + 1
             x = 0
-            while not village[m].isdigit() or m == len(village) - 1:
-                if village[m] == '=':
+            while m != len(village) - 1:
+                if village[m].isdigit():
+                    break
+                elif village[m] == '=':
                     x += 1
                 m += 1
             if village[m].isdigit() and int(village[i]) + int(village[m]) == 10:
                 b += 1
                 if x == 3:
                     a += 1
+                i = m - 1
+        i += 1
 
     if a == b and a != 0:
         return True
